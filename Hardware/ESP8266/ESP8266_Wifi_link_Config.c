1246fd28690692f491dfc2a8ed97834b4d4cfb19
happymiki <miki_859694663@sina.com>
2017-10-18 11:12:50 +0800

    add ESP8266_Wifi_link_Config file

---
+++ Hardware/ESP8266/ESP8266_Wifi_link_Config.c
@@ -0,0 +1,19 @@
+#include "ESP8266_Wifi_link_Config.h"
+
+bool IS_WIFI_LOG_DEBUG = true;
+
+char WIFI_SSID_NAME[] = "MERCURY_7A02";//此处填写wifi名称
+char WIFI_PASSWORD[] = "miki257056,,";//此处填写wifi密码
+
+/*
+char WIFI_SSID_NAME[] = "CML";//此处填写wifi名称
+char WIFI_PASSWORD[] = "qwertyuiop";//此处填写wifi密码
+*/
+//char TCP_SERVER_IPADDRESS[] = "TIAN";//此处可使用主机名和ip
+char TCP_SERVER_IPADDRESS[] = "115.159.203.38";
+char TCP_SERVER_PORT[] = "31511";
+char TCP_CILENT_IPADDRESS[] = "ESP_113AFD";
+char TCP_CILENT_PORT[] = "50000";
+
+
+
